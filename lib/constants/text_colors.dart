c02901ce9d659cb394ec17be2f3c9cff92981c70
l4mole <danielrojo9822@gmail.com>
2025-02-20 17:22:28 -0600

    Agregar clase TextStyles con estilos de texto para la aplicación

---
+++ lib/constants/text_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle appBarTitle = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle categoryTitle = TextStyle(
+    fontSize: 22,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle factText = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Colors.blueGrey,
+  );
+
+  static const TextStyle buttonText = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+}
